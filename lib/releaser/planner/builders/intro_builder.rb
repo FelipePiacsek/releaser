9b9325241694b51ac8421dcb8cf295c656e3b901
felipe <felipe.piacsek@gmail.com>
2018-12-26 18:35:18 -0200

    Removing unnecessary pull_requests parameter from IntroBuilder.

--- lib/releaser/planner/builders/intro_builder.rb
+++ lib/releaser/planner/builders/intro_builder.rb
@@ -3,11 +3,6 @@ require_relative 'base_template_builder'
 
 module Releaser
   class IntroBuilder < BaseTemplateBuilder
-
-    def initialize(pull_requests)
-      @pull_requests = pull_requests
-    end
-
     def build
       Releaser::Renderers.erb template_string!('partials/intro.txt.erb')
     end
--- test/unit/planner/builders/intro_builder_test.rb
+++ test/unit/planner/builders/intro_builder_test.rb
@@ -3,11 +3,9 @@ require_relative '../../../../lib/releaser/planner/builders/intro_builder'
 
 class IntroBuilderTest < BaseReleaserTest
 
-  test 'should correctly build intro template for given pull requests' do
-    pull_requests = [] # Intro does not use pr data at the moment.
-
+  test 'should correctly build intro template' do
     expected_output = template_file(INTRO_TEMPLATE_PATH).read
-    actual_output = Releaser::IntroBuilder.new(pull_requests).build
+    actual_output = Releaser::IntroBuilder.new.build
 
     assert_equal expected_output, actual_output
   end
