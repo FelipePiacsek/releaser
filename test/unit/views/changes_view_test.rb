115de7bb962f47e4356077b7d9109678b849dda0
felipe <felipe.piacsek@gmail.com>
2018-12-28 13:05:59 -0200

    Using pull request factory on ChangesViewTest.

--- test/unit/views/changes_view_test.rb
+++ test/unit/views/changes_view_test.rb
@@ -1,24 +1,13 @@
 require_relative '../../test_helper'
 require_relative '../../../lib/releaser/views/changes_view'
+require_relative '../../fixtures/factories/pull_request_factory'
 
 class ChangesViewTest < BaseReleaserTest
 
-  # TODO: Move me.
-  def build_pull_requests(scenario = GITHUB_4_UNRELEASED_3_RELEASED)
-    pr_file_content = fixture_file(scenario[:pull_requests]).read
-    prs_hash = JSON.parse(pr_file_content, symbolize_names: true)
-
-    prs_hash.map do |pr|
-      user_file_path = github_user_fixture_path "#{pr[:user][:id]}.json"
-      user_string = fixture_file(user_file_path).read
-      user = JSON.parse(user_string, symbolize_names: true)
-
-      PullRequest.new(pr, user)
-    end
-  end
+  include Factories::PullRequest
 
   def setup
-    @pull_requests = build_pull_requests
+    @pull_requests = pull_request_list GITHUB_4_UNRELEASED_3_RELEASED
   end
 
   test 'should correctly render changes template for non-empty pull requests array' do
