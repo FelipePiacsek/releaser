f2d4375b578fa0e1e8183e1392777c8b147e62d4
Felipe Piacsek <felipe.piacsek@gmail.com>
2019-04-04 19:41:50 -0300

    Removing NotImplementedError raising and adding actual implementation of #plan method.

--- lib/releaser/planner.rb
+++ lib/releaser/planner.rb
@@ -1,5 +1,6 @@
 require_relative '../../lib/releaser/clients/github_client'
 require_relative 'views/empty_release_view'
+require_relative 'views/full_release_view'
 
 ## Releaser plan builder.
 # Only exposes plan method. By default uses github as client.
@@ -14,12 +15,12 @@ module Releaser
     # Builds the release plan based on template partials, if there are unreleased pull requests.
     # Returns an "empty_release_template" if there is nothing to be released.
     def plan(repository_id)
-      prs = @client.unreleased_pull_requests repository_id
-      if prs.empty?
+      pull_requests = @client.unreleased_pull_requests repository_id
+      if pull_requests.empty?
         last_release = @client.tags(repository_id).first
         Releaser::EmptyReleaseView.new(last_release).render
       else
-        raise NotImplementedError
+        Releaser::FullReleaseView.new(pull_requests).render
       end
     end
 
@@ -28,5 +29,6 @@ module Releaser
     def default_client
       GithubClient.new
     end
+
   end
 end
